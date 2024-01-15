c2cd7a2ea711ab9eaff3bc663bc8f1c9ab664df8
1955815 <1955815@01HW1983165>
2024-01-15 23:34:42 +0530

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// yokk
+// yokk..
 public class DemoApplication {
 
 	public static void main(String[] args) {
