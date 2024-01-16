c80d1156b1e732c44135cbe6fac562878ac7369c
1955815 <1955815@172.18.232.99>
2024-01-16 18:41:34 +0530

    kill

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 // yokk..   
-// myName
+// myName....
 public class DemoApplication {
 
 	public static void main(String[] args) {
